d5b67d046b54d3e0a2abe6330bda9924fcc7904d
1120213028 <1120213028@bit.edu.cn>
2025-03-03 17:56:05 +0800

    add Feature2

--- Main.java
+++ Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
+        Feature2.calculate();
+    }
+}
+
+class Feature2 {
+    public static int calculate() {
+        return 42;
     }
 }
